11852ac33e5c5719c2fed9240b826b5ecc91e93b
Nic Mayer <nicmayer42@gmail.com>
2019-10-13 14:21:55 +0100

    Can't press multiple .

--- script.js
+++ script.js
@@ -44,14 +44,17 @@ TODO:
 
     // When: Number key is pressed. Get the current number selected
     var setNumKey = function(keyPressed) {
-      if (resultNum) { // If a result was displayed, reset number
-        theNum = keyPressed;
-        resultNum = "";
-      } else { // Otherwise, add digit to previous number (this is a string!)
-        theNum += keyPressed;
-      }
+      //if . was pressed, then only do this if theNum doesn't already include a .
+      if ((keyPressed ==="." && theNum.indexOf(".") === -1) || keyPressed !=="."){
+        if (resultNum) { // If a result was displayed, reset number
+          theNum = keyPressed;
+          resultNum = "";
+        } else { // Otherwise, add digit to previous number (this is a string!)
+          theNum += keyPressed;
+        }
 
-       viewer.innerHTML = theNum; // Display current number
+         viewer.innerHTML = theNum; // Display current number
+       }
     }
 
     // When: Operator is clicked. Pass number to oldNum and save operator
